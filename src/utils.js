3bb1e5cb7275eef9b8cedc1b6dbe86b4778bcf69
Lyle Simmons <lylesimmons125@gmail.com>
2024-03-24 16:54:07 -0400

    lab complete

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
-// Your code here
+export function pointsForWord(word) {
+    let points = 0;
+    for (const char of word) {
+      points += /[aeiou]/.test(char) ? 1 : 2;
+    }
+    return points;
+  }
